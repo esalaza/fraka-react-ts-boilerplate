62e1a2c9f82132485c4d955d5ae6763566677a7b
Esteban Salazar <esteban.salazar@terem.com.au>
2020-02-23 11:03:58 +1100

    Migrate to Parcel 👌

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,10 @@ import * as ReactDOM from "react-dom";
 
 import { Hello } from "./components/Hello";
 
+if (module.hot) {
+  module.hot.accept();
+}
+
 ReactDOM.render(
   <Hello compiler="TypeScript" framework="React" />,
   document.getElementById("example")
